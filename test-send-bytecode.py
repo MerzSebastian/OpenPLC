0107104954a5e96b51f31f13a41e1cb7ff6a660e
MerzSebastian <merz.sebastian95@gmail.com>
2025-09-07 14:39:03 +0200

    moved from python to doing it directly in the browser

--- test-send-bytecode.py
+++
@@ -1,5 +0,0 @@
-import serial
-ser = serial.Serial('COM3', 9600)  # Replace 'COM3' with your port
-bytecode = [9, 2, 10, 4, 9, 3, 1, 2, 0, 1, 3, 1, 3, 0, 2, 4, 2, 1, 3, 2, 4, 3]
-ser.write(bytearray(bytecode))
-ser.close()
